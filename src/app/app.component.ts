2bef6a17da4f9d9fea3632777ee55d16f8ebba89
Kyle Bussert <kyle.bussert@gmail.com>
2022-04-01 08:22:11 -0400

    Adding simple app component layout with minimal styling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'typing';
+  targetSentence = lorem.sentence();
 }
